767a350b2545f84a0125b58df049d146e1f18e45
kasmr <alikkras@gmail.com>
2020-01-03 04:26:03 +0300

    added animation

--- ruletla/main.js
+++ ruletla/main.js
@@ -1,26 +1,25 @@
-const ball1 = document.getElementById('ball1');
-const ball2 = document.getElementById('ball2');
-const ball3 = document.getElementById('ball3');
-const balls = document.querySelectorAll('.ball');
 const spin = document.querySelector('.spin');
+const wheel = document.getElementsByTagName('img')[0];
 
-let count = 1;
+let count = 0;
 function countIncrease(e) {
   count++;
   console.log(count);
-  if (count === 1) {
-    ball1.style.display = 'block';
+  switch (count) {
+    case 1:
+      wheel.classList.add('rotate1');
+      break;
+    case 2:
+      wheel.classList.add('rotate2');
+      break;
+    case 3:
+      wheel.classList.add('rotate3');
+      break;
   }
-  if (count === 2) {
-    ball1.style.display = 'none';
-    ball2.style.display = 'block';
-  }
-  if (count === 3) {
-    ball2.style.display = 'none';
-    ball3.style.display = 'block';
-  }
+
   if (count > 3) {
-    balls.forEach(balls => (balls.style.display = 'none'));
+    alert(`Игра окончена`);
+    wheel.classList.remove('rotate1', 'rotate2', 'rotate3');
     count = 0;
   }
 }
